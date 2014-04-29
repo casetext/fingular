49c8de8e0b4ebd77cb4dcfda15aeb830a709cb73
Harry Schmidt <goldibex@casetext.com>
2014-04-28 19:55:27 -0700

    RC1

--- fingular.js
+++ fingular.js
@@ -1,29 +1,87 @@
 'use strict';
 
-(function(angular) {
-angular.module('firebase', [])
-  .provider('$firebase', function FirebaseProvider() {
-    this.setDomain = function(newDomain) {
-      this.firebaseDomain = newDomain;
+(function(angular, Firebase) {
+  angular.module('fingular', [])
+  .provider('$firebaseRef', function FirebaseRefProvider() {
+    var firebaseDomain
+      , protocol = 'https'
+      , mockMode
+      , mockFirebase
+      , mockData = {};
+
+    this['domain'] = function(newDomain) {
+      firebaseDomain = newDomain;
+      return this;
+    };
+
+    this['protocol'] = function(newProtocol) {
+      switch(protocol) {
+      case 'http':
+      case 'https':
+        protocol = newProtocol;
+        break;
+      default:
+        throw new Error('Invalid protocol. Only http and https can be used!');
+        break;
+      }
+      return this;
+    }
+
+    this['mockWith'] = function(mocker) {
+      mockFirebase = mocker;
+      mockMode = true;
+      return this;
+    };
+
+    this['mockOut'] = function(path, data) {
+      mockData[path] = data;
+      return this;
     };
 
     this.$get = ['$injector', function($injector) {
-      if (!(this.firebaseDomain instanceof String)) {
+      if (!(typeof(firebaseDomain) === 'string')) {
         if ($injector.has('firebaseDomain')) {
-          this.firebaseDomain = $injector.get('firebaseDomain');
+          firebaseDomain = $injector.get('firebaseDomain');
         } else {
           throw new Error('You must supply the domain name of your ' +
                           'Firebase account, either by providing the ' +
                           'constant \'firebaseDomain\' or by setting ' +
                           'the \'firebaseDomain\' property on the $firebaseProvider!');
         }
+        if ($injector.has('firebaseMock')) {
+          mockFirebase = injector.get('firebaseMock');
+          mockMode = true;
+        }
+        if ($injector.has('firebaseMockData')) {
+          angular.forEach($injector.get('firebaseMockData'), function(mockDatum, key) {
+            mockData[key] = mockDatum;
+          });
+        }
       }
 
       var injectable = function(path) {
+        if (path[0] !== '/') {
+          path = '/' + path;
+        }
+
+        if (mockMode) {
+          return new mockFirebase('Mock://' + firebaseDomain + path, mockData[path]);
+        } else {
+          return new Firebase(protocol + '://' + firebaseDomain + path);
+        }
       };
-      injectable.domain = this.firebaseDomain;
+      injectable.protocol = protocol;
+      injectable.domain = firebaseDomain;
+      injectable.mocked = mockMode === true;
+
       return injectable;
+    }];
+  })
+  .provider('$firebaseUser', function FirebaseUserProvider() {
+
+    this.$get = ['$injector', function($injector) {
+
     }];
   });
 
-})(window.angular);
+})(window.angular, window.Firebase);
--- test/spec/index.js
+++ test/spec/index.js
@@ -1,70 +1,123 @@
-describe('the "firebase" module', function() {
+MockFirebase.DEFAULT_DATA = {
+};
+
+describe('the "fingular" module', function() {
+
   it('exists', function() {
     expect(function() {
-      module('firebase');
+      module('fingular');
     }).not.to.Throw();
   });
+
 });
 
-describe('the "$firebase" provider', function() {
+describe('the "$firebaseRef" provider', function() {
+
   it(
     'fails without the firebaseDomain property on firebaseProvider ' +
     'or the firebaseDomain constant', function() {
-    module('firebase');
+    module('fingular');
 
     expect(function() {
-      inject(function($firebase) {});
+      inject(function($firebaseRef) {});
     }).to.Throw();
   });
 
-  it('succeeds when the firebaseDomain constant is provided', function() {
+  it('succeeds when the firebaseDomain constant is provided', function(done) {
 
     module(function($provide) {
       $provide.constant('firebaseDomain', 'woot.firebaseio.com');
     });
 
-    module('firebase');
+    module('fingular');
     expect(function() {
-      inject(function($firebase) {});
+      inject(function($firebaseRef) {});
     }).not.to.Throw();
+    done();
+
   });
 
   describe('in a config block', function() {
+
     beforeEach(function() {
-      angular.module('test', ['firebase'])
-      .config(function($firebaseProvider) {
-        $firebaseProvider.setDomain('override.firebaseio.com');
+      angular.module('test', ['fingular'])
+      .config(function($firebaseRefProvider) {
+        $firebaseRefProvider.domain('override.firebaseio.com');
       })
-      .service('fb', function($firebase) {
-        return $firebase;
+      .service('fb', function($firebaseRef) {
+        return $firebaseRef;
       });
     });
 
-    it('succeeds when firebaseDomain is set on $firebaseProvider', function() {
-
+    it('succeeds when firebaseDomain is set on $firebaseRefProvider', function(done) {
       module('test');
       expect(function() {
         inject(function(fb) {});
       }).not.to.Throw();
+      done();
     });
+
   });
 
   describe('with both configuration paths used', function() {
+
     beforeEach(function() {
-      angular.module('test2', ['firebase'])
+      angular.module('test2', ['fingular'])
       .constant('firebaseDomain', 'woot.firebaseio.com')
-      .config(function($firebaseProvider) {
-        $firebaseProvider.setDomain('override.firebaseio.com');
+      .config(function($firebaseRefProvider) {
+        $firebaseRefProvider.domain('override.firebaseio.com');
       })
-      .service('fb2', function($firebase) {
-        return $firebase;
+      .service('fb2', function($firebaseRef) {
+        return $firebaseRef;
       });
     });
 
-    it('prefers the value set on $firebaseProvider', function() {
+    it('prefers the value set on $firebaseRefProvider', function(done) {
       module('test2');
       inject(function(fb2) {
         expect(fb2.domain).to.equal('override.firebaseio.com');
+        done();
+      });
+    });
+
+  });
+
+  describe('when mock mode is set', function() {
+
+    it('distributes instances of the provided mocking prototype function', function() {
+      angular.module('test3', ['fingular'])
+      .config(function($firebaseRefProvider) {
+        $firebaseRefProvider
+        .domain('test.foo.com')
+        .mockWith(MockFirebase);
+      })
+      .service('fb3', function($firebaseRef) {
+        return $firebaseRef('/foo/bar/baz');
+      });
+      module('test3');
+      inject(function(fb3) {
+        expect(fb3).to.be.an.instanceof(MockFirebase);
+      });
+    });
+
+    it('accepts mock data via the "mockOut" method on the provider', function() {
+      angular.module('test5', ['fingular'])
+      .config(function($firebaseRefProvider) {
+        $firebaseRefProvider
+        .domain('test.foo.com')
+        .mockOut('/foo', 'bar')
+        .mockWith(MockFirebase);
+      })
+      .service('fb5', function($firebaseRef) {
+        return $firebaseRef;
+      });
+      module('test5');
+      inject(function(fb5) {
+        var fb = fb5('/foo');
+        fb.on('value', function(snapshot) {
+          expect(snapshot.val()).to.equal('bar');
+        });
+        fb.flush();
       });
     });
   });
